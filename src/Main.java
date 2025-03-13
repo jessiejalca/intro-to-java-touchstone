108fefc01a340c25847d74f604dc6ccb5d9d9d25
Jessie Jalca <jessiejalca@gmail.com>
2025-03-13 01:41:12 -0400

    feat: rename files with confirmation, print results, ignore non-valid files

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
         ArrayList<FileToRename> files = new ArrayList<>();
 
         // Get the contents of a directory
-        System.out.print("Enter the directory path containing the files to rename: ");
+        System.out.print("Enter /path/to/rename/files: ");
         String directoryPath = input.nextLine();
         File directory = new File(directoryPath);
         File[] contents = directory.listFiles();
@@ -32,9 +32,9 @@ public class Main {
             System.exit(1);
         }
 
-        // Collect ONLY files
+        // Collect ONLY valid files
         for (File item : contents) {
-            if (item.isFile()) {
+            if (item.isFile() && item.getName().lastIndexOf(".") > 0) {
                 files.add(new FileToRename(item));
             }
         }
@@ -45,17 +45,13 @@ public class Main {
             System.exit(0);
         }
 
-        // Rename files
-        System.out.println("The following files will be changed:");
+        // Store and print queued name changes
+        System.out.println("\nRenaming the following files:");
         HashMap<String, Integer> extensions = new HashMap<>();
         for (FileToRename file : files) {
             // Get file extension
             String filename = file.getOldName();
-            String extension = "";
-            int extIndex = filename.lastIndexOf(".");
-            if (extIndex > 0) {
-                extension = filename.substring(extIndex + 1);
-            }
+            String extension = filename.substring(filename.lastIndexOf(".") + 1);
 
             // Update and get extension count
             if (extensions.containsKey(extension)) {
@@ -69,13 +65,25 @@ public class Main {
             file.setNewName(String.format(filenameFmt, extensions.get(extension), extension));
 
             // Print name changes
-            System.out.printf("%-14s --> %20s\n", file.getOldName(), file.getNewName());
+            System.out.println(file.getOldName() + " --> " + file.getNewName());
+        }
+
+        // Confirm changes
+        System.out.print("Confirm changes (y/n): ");
+        int filesChanged = 0;
+        if (input.nextLine().equalsIgnoreCase("y")) {
+            // Rename files if user approves
+            for (FileToRename file : files) {
+                if (file.rename()) filesChanged++;
+            }
+        } else {
+            // Exit without renaming any files
+            System.out.println("\nChanges cancelled.");
+            System.exit(0);
         }
 
         // Print results
-        System.out.println("\nNumber of files by extension:");
-        for (String extension : extensions.keySet()) {
-            System.out.printf("%8s:%4d\n", extension, extensions.get(extension));
-        }
+        System.out.println("\n"+ filesChanged + " files renamed.");
+        System.out.println((files.size() - filesChanged) + " errors.");
     }
 }
